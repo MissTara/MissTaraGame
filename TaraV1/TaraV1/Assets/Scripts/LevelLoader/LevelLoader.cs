b0a38aa9d8b7afcbbdec0545818f6689ad5928e9
Dexter <wuwenhuang88@qq.com>
2013-09-25 11:06:44 -0400

    delete level loader not want codes

--- TaraV1/TaraV1/Assets/Scripts/LevelLoader/LevelLoader.cs
+++ TaraV1/TaraV1/Assets/Scripts/LevelLoader/LevelLoader.cs
@@ -37,28 +37,12 @@ public class LevelLoader : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-        //if (GameManager.isPaused == false && levelLoaded == false)
-        //{
-        //    for (int it = 0; it < howManyBuildings; it++)
-        //    {
-        //        Building tempBuild;
-        //        Vector3 tempBuildPosition = new Vector3(-65, 90, initialPositionBuilding + distanceBetweenBuildings);
-        //        tempBuild = Instantiate(Resources.Load("Buildings/building1", typeof(Building)), tempBuildPosition, Quaternion.Euler(0, 270, 0)) as Building;
-        //        //tempBuild.transform.localPosition = new Vector3(-65, 90, initialPositionBuilding + distanceBetweenBuildings);
-        //        Debug.Log(tempBuild);
-        //        buildings.Add(tempBuild as Building);
-
-        //        distanceBetweenBuildings += distanceBetweenBuildings;
-        //    }
-        //    levelLoaded = true;
-        //}
         if (GameManager.isPaused == false)
         {
             planes = GeometryUtility.CalculateFrustumPlanes(camera);
             foreach (GameObject building in buildings)
             {
                 if (!building.gameObject.activeSelf && building.gameObject.transform.localPosition.z < camera.gameObject.transform.localPosition.z)
-                    //(GeometryUtility.TestPlanesAABB(planes, building.collider.bounds) && Physics.Raycast(camera.ScreenPointToRay(camera.gameObject.transform.position + new Vector3(0,0,-100)),out hit, camera.farClipPlane))
                 {
                     
                     Debug.Log(building.name + " has been detected!");
